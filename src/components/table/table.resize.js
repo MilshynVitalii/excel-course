74e54bc46127502155a12b9c66fbacd6dd2618cf
MilshynVitalii <milshyn.vitalii@gmail.com>
2021-02-09 19:02:40 +0200

    Finish table resize

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,34 @@
+import {$} from '@core/dom';
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target);
+  const $parent = $resizer.closest('[data-type="resizable"]');
+  const coords = $parent.getCoords();
+  const type = $resizer.data.resize;
+  let value;
+  let delta;
+  document.onmousemove = e => {
+    if (type === 'col') {
+      delta = e.pageX - coords.right;
+      value = coords.width + delta;
+      $resizer.css({right: -delta + 'px'});
+    } else {
+      delta = e.pageY - coords.bottom;
+      value = coords.height + delta;
+      $resizer.css({bottom: -delta + 'px'});
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+    if (type === 'col') {
+      $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => el.style.width = value + 'px');
+      $resizer.css({right: 0});
+    } else {
+      $parent.css({height: value + 'px'});
+      $resizer.css({bottom: 0});
+    }
+  }
+}
